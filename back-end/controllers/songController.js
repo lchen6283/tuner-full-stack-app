d756bb84a58b7569092d9b3c2d11513ed982d3d5
lchen6283 <lchen6283@gmail.com>
2022-08-04 13:50:09 -0400

    delete/update/validate

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const songs = express.Router();
-const db = require("../db/dbConfig");
+// const db = require("../db/dbConfig");
+const {
+  getAllSongs,
+  getASong,
+  createSong,
+  deleteSong,
+  updateSong,
+} = require("../queries/songs");
+
+const {
+  checkBoolean,
+  checkName,
+  checkForNoAdditionalParams,
+} = require("../validations/checkSongs");
 
 //Index
 songs.get("/", async (req, res) => {
-  const allSongs = await db.any("SELECT * FROM songs");
+  const allSongs = await getAllSongs();
   console.log(allSongs);
   res.json(allSongs);
 });
@@ -13,7 +26,7 @@ songs.get("/", async (req, res) => {
 songs.get("/:id", async (req, res) => {
   console.log(req.params.id);
   const { id } = req.params;
-  const song = await db.one("SELECT * FROM songs WHERE id=$1", id);
+  const song = await getASong(id);
   if (song) {
     res.status(200).json(song);
   } else {
@@ -25,17 +38,40 @@ songs.get("/:id", async (req, res) => {
 songs.post("/new", async (req, res) => {
   const newSong = req.body;
   console.log(newSong);
-  const newSongs = await db.any(
-    "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-    [
-      req.body.name,
-      req.body.artist,
-      req.body.album,
-      req.body.time,
-      req.body.is_favorite,
-    ]
-  );
+  const newSongs = await createSong(newSong);
   res.status(200).json(newSongs);
 });
+
+//Delete
+songs.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedSong = await deleteSong(id);
+  if (deletedSong) {
+    if (deletedSong.id) {
+      res.status(200).json(deletedSong);
+    } else {
+      res.status(404).json({ error: "Song not found" });
+    }
+  } else {
+    console.error(deletedSong);
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+//Update
+songs.put(
+  "/:id",
+  checkBoolean,
+  checkName,
+  checkForNoAdditionalParams,
+  async (req, res) => {
+    try {
+      const song = await updateSong(req.params.id, req.body);
+      res.json(song);
+    } catch (error) {
+      res.status(400).json({ error: error });
+    }
+  }
+);
 
 module.exports = songs;
--- back-end/queries/songs.js
+++ back-end/queries/songs.js
@@ -1,13 +1,16 @@
 const db = require("../db/dbConfig");
 
+//GET ALL
 const getAllSongs = async () => {
   try {
-    return songs;
+    const allSongs = await db.any("SELECT * FROM songs");
+    return allSongs;
   } catch (error) {
     return error;
   }
 };
 
+//GET INDIVIDUAL
 const getASong = async (id) => {
   try {
     const song = await db.one("SELECT * FROM songs WHERE id=$1", id);
@@ -17,7 +20,55 @@ const getASong = async (id) => {
   }
 };
 
+//CREATE
+const createSong = async ({ name, artist, album, time, is_favorite }) => {
+  try {
+    const newSongs = await db.any(
+      "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [name, artist, album, time, is_favorite]
+    );
+    return newSongs;
+  } catch (error) {
+    return error;
+  }
+};
+
+//DELETE
+const deleteSong = async (id) => {
+  try {
+    const oneSong = await db.one(
+      "DELETE FROM songs WHERE id=$1 RETURNING *",
+      id
+    );
+    return oneSong;
+  } catch (error) {
+    console.log(error.message || error);
+    return error;
+  }
+};
+
+//UPDATE
+const updateSong = async (
+  id,
+  { name, artist, album, time, is_favorite, ...otherStuff }
+) => {
+  console.log(id, name, artist, album, time, is_favorite, otherStuff);
+  try {
+    const updateSong = await db.one(
+      "UPDATE songs SET name=$1, artist=$2, album=$3, time=$4, is_favorite=$5 where id=$6 RETURNING *",
+      [name, artist, album, time, is_favorite, id]
+    );
+    return updateSong;
+  } catch (error) {
+    console.log(error.message || error);
+    return error;
+  }
+};
+
 module.exports = {
   getAllSongs,
   getASong,
+  createSong,
+  deleteSong,
+  updateSong,
 };
